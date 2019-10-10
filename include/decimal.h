941beed1999bf5c02183f10e6b0770acfda1ae48
Brandon Cecala <brandon@sandhaven.tech>
2019-10-10 15:54:38 -0600

    Moved rounding mode to the constructors.

--- include/decimal.h
+++ include/decimal.h
@@ -32,91 +32,85 @@
 #include <random>
 #include <sstream>
 
+#include <bid_conf.h> // Intel's definitions
+
+namespace decimal754 {
+
+typedef uint64_t D64; // holds 64-bit decimals
+typedef struct __attribute__((aligned(16))) { uint64_t w[2]; } D128; // holds 128-bit decimals
+typedef unsigned int RoundMode;
+typedef unsigned int ErrorFlags;
+
+// Declare the C functions we will be linking from Intel's library (libbid.a)
 extern "C" {
-#include <bid_conf.h>
-
-typedef uint64_t D64;
-typedef struct __attribute__((aligned(16))) { unsigned long long w[2]; } D128;
-
-/* rounding modes */
-typedef enum _IDEC_roundingmode {
-    _IDEC_nearesteven = 0,
-    _IDEC_downward    = 1,
-    _IDEC_upward      = 2,
-    _IDEC_towardzero  = 3,
-    _IDEC_nearestaway = 4,
-    _IDEC_dflround    = _IDEC_nearesteven
-} _IDEC_roundingmode;
-typedef unsigned int _IDEC_round;
-
-/* exception flags */
-typedef enum _IDEC_flagbits {
-    _IDEC_invalid       = 0x01,
-    _IDEC_zerodivide    = 0x04,
-    _IDEC_overflow      = 0x08,
-    _IDEC_underflow     = 0x10,
-    _IDEC_inexact       = 0x20,
-    _IDEC_allflagsclear = 0x00
-} _IDEC_flagbits;
-typedef unsigned int _IDEC_flags;  // could be a struct with diagnostic info
 
 /* 128-bit functions */
-extern D128 __bid128_from_string (char *ps, _IDEC_round rnd_mode, _IDEC_flags *pfpsf);
+extern D128 __bid128_from_string (char *ps, RoundMode rnd_mode, ErrorFlags *pfpsf);
 extern D128 __bid128_from_uint32 (uint32_t);
 extern D128 __bid128_from_uint64 (uint64_t);
 extern D128 __bid128_from_int32 (int32_t);
 extern D128 __bid128_from_int64 (int64_t);
 extern D128 __bid128_abs (D128 x);
 extern D128 __bid128_negate (D128 x);
-extern D128 __bid128_add ( D128, D128, _IDEC_round, _IDEC_flags *);
-extern D128 __bid128_sub ( D128, D128, _IDEC_round, _IDEC_flags *);
-extern D128 __bid128_mul ( D128, D128, _IDEC_round, _IDEC_flags *);
-extern D128 __bid128_div ( D128, D128, _IDEC_round, _IDEC_flags *);
-extern void __bid128_to_string ( char *ps, D128 x, _IDEC_flags *pfpsf);
-extern uint8_t __bid128_to_uint8_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern uint16_t __bid128_to_uint16_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern uint32_t __bid128_to_uint32_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern uint64_t __bid128_to_uint64_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern int8_t __bid128_to_int8_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern int16_t __bid128_to_int16_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern int32_t __bid128_to_int32_xrnint (D128 x, _IDEC_flags *pfpsf);
-extern int64_t __bid128_to_int64_xrnint (D128 x, _IDEC_flags *pfpsf);
+extern D128 __bid128_add ( D128, D128, RoundMode, ErrorFlags *);
+extern D128 __bid128_sub ( D128, D128, RoundMode, ErrorFlags *);
+extern D128 __bid128_mul ( D128, D128, RoundMode, ErrorFlags *);
+extern D128 __bid128_div ( D128, D128, RoundMode, ErrorFlags *);
+extern void __bid128_to_string ( char *ps, D128 x, ErrorFlags *pfpsf);
+extern uint8_t __bid128_to_uint8_xrnint (D128 x, ErrorFlags *pfpsf);
+extern uint16_t __bid128_to_uint16_xrnint (D128 x, ErrorFlags *pfpsf);
+extern uint32_t __bid128_to_uint32_xrnint (D128 x, ErrorFlags *pfpsf);
+extern uint64_t __bid128_to_uint64_xrnint (D128 x, ErrorFlags *pfpsf);
+extern int8_t __bid128_to_int8_xrnint (D128 x, ErrorFlags *pfpsf);
+extern int16_t __bid128_to_int16_xrnint (D128 x, ErrorFlags *pfpsf);
+extern int32_t __bid128_to_int32_xrnint (D128 x, ErrorFlags *pfpsf);
+extern int64_t __bid128_to_int64_xrnint (D128 x, ErrorFlags *pfpsf);
 extern int __bid128_isSigned (D128 x);
-extern int __bid128_quiet_equal (D128 x, D128 y, _IDEC_flags *pfpsf);
-extern int __bid128_quiet_less (D128 x, D128 y, _IDEC_flags *pfpsf); // TODO: SIGNALLING?
+extern int __bid128_quiet_equal (D128 x, D128 y, ErrorFlags *pfpsf);
+extern int __bid128_quiet_less (D128 x, D128 y, ErrorFlags *pfpsf); // TODO: SIGNALLING?
 
 /* 64-bit functions */
-extern D64 __bid64_from_string (char *ps, _IDEC_round rnd_mode, _IDEC_flags *pfpsf);
+extern D64 __bid64_from_string (char *ps, RoundMode rnd_mode, ErrorFlags *pfpsf);
 extern D64 __bid64_from_uint32 (uint32_t);
 extern D64 __bid64_from_uint64 (uint64_t);
 extern D64 __bid64_from_int32 (int32_t);
 extern D64 __bid64_from_int64 (int64_t);
 extern D64 __bid64_abs (D64 x);
 extern D64 __bid64_negate (D64 x);
-extern D64 __bid64_add ( D64, D64, _IDEC_round, _IDEC_flags *);
-extern D64 __bid64_sub ( D64, D64, _IDEC_round, _IDEC_flags *);
-extern D64 __bid64_mul ( D64, D64, _IDEC_round, _IDEC_flags *);
-extern D64 __bid64_div ( D64, D64, _IDEC_round, _IDEC_flags *);
-extern void __bid64_to_string ( char *ps, D64 x, _IDEC_flags *pfpsf);
-extern uint8_t __bid64_to_uint8_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern uint16_t __bid64_to_uint16_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern uint32_t __bid64_to_uint32_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern uint64_t __bid64_to_uint64_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern int8_t __bid64_to_int8_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern int16_t __bid64_to_int16_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern int32_t __bid64_to_int32_xrnint (D64 x, _IDEC_flags *pfpsf);
-extern int64_t __bid64_to_int64_xrnint (D64 x, _IDEC_flags *pfpsf);
+extern D64 __bid64_add ( D64, D64, RoundMode, ErrorFlags *);
+extern D64 __bid64_sub ( D64, D64, RoundMode, ErrorFlags *);
+extern D64 __bid64_mul ( D64, D64, RoundMode, ErrorFlags *);
+extern D64 __bid64_div ( D64, D64, RoundMode, ErrorFlags *);
+extern void __bid64_to_string ( char *ps, D64 x, ErrorFlags *pfpsf);
+extern uint8_t __bid64_to_uint8_xrnint (D64 x, ErrorFlags *pfpsf);
+extern uint16_t __bid64_to_uint16_xrnint (D64 x, ErrorFlags *pfpsf);
+extern uint32_t __bid64_to_uint32_xrnint (D64 x, ErrorFlags *pfpsf);
+extern uint64_t __bid64_to_uint64_xrnint (D64 x, ErrorFlags *pfpsf);
+extern int8_t __bid64_to_int8_xrnint (D64 x, ErrorFlags *pfpsf);
+extern int16_t __bid64_to_int16_xrnint (D64 x, ErrorFlags *pfpsf);
+extern int32_t __bid64_to_int32_xrnint (D64 x, ErrorFlags *pfpsf);
+extern int64_t __bid64_to_int64_xrnint (D64 x, ErrorFlags *pfpsf);
 extern int __bid64_isSigned (D64 x);
-extern int __bid64_quiet_equal (D64 x, D64 y, _IDEC_flags *pfpsf);
-extern int __bid64_quiet_less (D64 x, D64 y, _IDEC_flags *pfpsf);
+extern int __bid64_quiet_equal (D64 x, D64 y, ErrorFlags *pfpsf);
+extern int __bid64_quiet_less (D64 x, D64 y, ErrorFlags *pfpsf);
 }
 
-namespace decimal754 {
-	
+// Base class for decimal types
 template <class T>
 class Decimal {
 protected:
-	T val;
+	T val; // either D64 or D128
+
+	// Rounding modes
+	enum Round: unsigned int{
+		NearestEven = 0,	// 3.1415 -> 3.142,		2.71828182845 -> 2.7182818284
+		Downward    = 1,	// -3.1415 -> -3.142	3.1415 -> 3.141
+		Upward      = 2,	// -3.1415 -> 3.141     3.1415 -> 3.142
+		TowardZero  = 3,	// -3.1415 -> -3.141	3.1415 -> 3.141
+		NearestAway = 4		// 3.1415 -> 3.142,		2.71828182845 -> 2.7182818285
+	};
+
+	RoundMode round_mode = Round::NearestEven;
 
 	enum Error: unsigned int {
 		None			= 0x00,
@@ -127,54 +121,54 @@ protected:
 		Inexact			= 0x20
 	};
 	
-	unsigned int errors = Error::None;
+	ErrorFlags errors = Error::None;
 			
-	void check_flags(const _IDEC_flags flags) const {
+	void check_flags(const ErrorFlags flags) const {
 		if ((flags & Error::Invalid) == Error::Invalid) {
 			throw Decimal::Exception("Invalid decimal operation", flags);
 		}
 	}
 
-	void save_flags(const _IDEC_flags flags) {
+	void save_flags(const ErrorFlags flags) {
 		this->errors |= flags;
 		this->check_flags(flags);
 	}
 
 	std::function<T(const uint32_t)> from_uint32;
 	std::function<T(const uint64_t)> from_uint64;
-	std::function<T(char*, _IDEC_round, _IDEC_flags*)> from_string;
+	std::function<T(char*, RoundMode, ErrorFlags*)> from_string;
 	std::function<T(int32_t)> from_int32;
 	std::function<T(int64_t)> from_int64;
-	std::function<void(char*, T, _IDEC_flags*)> to_string;
-	std::function<uint8_t(T, _IDEC_flags*)> to_uint8_xrnint;
-	std::function<uint16_t(T, _IDEC_flags*)> to_uint16_xrnint;
-	std::function<uint32_t(T, _IDEC_flags*)> to_uint32_xrnint;
-	std::function<uint64_t(T, _IDEC_flags*)> to_uint64_xrnint;
-	std::function<int8_t(T, _IDEC_flags*)> to_int8_xrnint;
-	std::function<int16_t(T, _IDEC_flags*)> to_int16_xrnint;
-	std::function<int32_t(T, _IDEC_flags*)> to_int32_xrnint;
-	std::function<int64_t(T, _IDEC_flags*)> to_int64_xrnint;
+	std::function<void(char*, T, ErrorFlags*)> to_string;
+	std::function<uint8_t(T, ErrorFlags*)> to_uint8_xrnint;
+	std::function<uint16_t(T, ErrorFlags*)> to_uint16_xrnint;
+	std::function<uint32_t(T, ErrorFlags*)> to_uint32_xrnint;
+	std::function<uint64_t(T, ErrorFlags*)> to_uint64_xrnint;
+	std::function<int8_t(T, ErrorFlags*)> to_int8_xrnint;
+	std::function<int16_t(T, ErrorFlags*)> to_int16_xrnint;
+	std::function<int32_t(T, ErrorFlags*)> to_int32_xrnint;
+	std::function<int64_t(T, ErrorFlags*)> to_int64_xrnint;
 	std::function<int(T)> is_signed;
-	std::function<int(T, T, _IDEC_flags*)> quiet_equal;
-	std::function<int(T, T, _IDEC_flags*)> quiet_less;
+	std::function<int(T, T, ErrorFlags*)> quiet_equal;
+	std::function<int(T, T, ErrorFlags*)> quiet_less;
 	std::function<T(T)> abs;
 	std::function<T(T)> negate;
-	std::function<T(T, T, _IDEC_round, _IDEC_flags*)> add;
-	std::function<T(T, T, _IDEC_round, _IDEC_flags*)> sub;
-	std::function<T(T, T, _IDEC_round, _IDEC_flags*)> mul;
-	std::function<T(T, T, _IDEC_round, _IDEC_flags*)> div;
+	std::function<T(T, T, RoundMode, ErrorFlags*)> add;
+	std::function<T(T, T, RoundMode, ErrorFlags*)> sub;
+	std::function<T(T, T, RoundMode, ErrorFlags*)> mul;
+	std::function<T(T, T, RoundMode, ErrorFlags*)> div;
 	
 	template <class TResult>
-	TResult invoke(std::function<TResult(_IDEC_flags*)> func) {
-		_IDEC_flags flags = _IDEC_allflagsclear;
+	TResult invoke(std::function<TResult(ErrorFlags*)> func) {
+		ErrorFlags flags = Error::None;
 		TResult result = func(&flags);
 		this->save_flags(flags);
 		return result;
 	}
 	
 	template <class TResult>
-	TResult invoke(std::function<TResult(_IDEC_flags*)> func) const {
-		_IDEC_flags flags = _IDEC_allflagsclear;
+	TResult invoke(std::function<TResult(ErrorFlags*)> func) const {
+		ErrorFlags flags = Error::None;
 		TResult result = func(&flags);
 		check_flags(flags);
 		return result;
@@ -190,13 +184,15 @@ protected:
 			delete[] this->val;
 		}
 	};
+
+	Decimal(const RoundMode & round_mode = Round::NearestEven) : round_mode(round_mode) {}
 	
 public:	 
 	struct Exception : public std::runtime_error {
-		_IDEC_flags flags;
+		ErrorFlags flags;
 
-		Exception(const std::string & message, _IDEC_flags flags) : std::runtime_error(message), flags(flags) {}
-		Exception(const char * message, _IDEC_flags flags) : std::runtime_error(message), flags(flags) {}
+		Exception(const std::string & message, ErrorFlags flags) : std::runtime_error(message), flags(flags) {}
+		Exception(const char * message, ErrorFlags flags) : std::runtime_error(message), flags(flags) {}
 		
 	};
 
@@ -235,7 +231,7 @@ public:
 		char buffer[64];
 		buffer[63] = '\0';
 		
-		_IDEC_flags flags = _IDEC_allflagsclear;
+		ErrorFlags flags = Error::None;
 		this->to_string (buffer, this->val, &flags);
 		check_flags(flags);
 
@@ -261,92 +257,93 @@ public:
 	}
 
 	explicit operator unsigned char() const { 
-		return this->invoke<unsigned char>([&](_IDEC_flags * flags) {
+		return this->invoke<unsigned char>([&](ErrorFlags * flags) {
 			return this->to_uint8_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator unsigned short() const { 
-		return this->invoke<unsigned short>([&](_IDEC_flags * flags) {
+		return this->invoke<unsigned short>([&](ErrorFlags * flags) {
 			return this->to_uint16_xrnint (this->val, flags);
 		});
 	}
 
 	explicit operator unsigned int() const { 
-		return this->invoke<unsigned int>([&](_IDEC_flags * flags) {
+		return this->invoke<unsigned int>([&](ErrorFlags * flags) {
 			return this->to_uint32_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator unsigned long() const { 
-		return this->invoke<unsigned long>([&](_IDEC_flags * flags) {
+		return this->invoke<unsigned long>([&](ErrorFlags * flags) {
 			return this->to_uint64_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator unsigned long long() const { 
-		return this->invoke<unsigned long long>([&](_IDEC_flags * flags) {
+		return this->invoke<unsigned long long>([&](ErrorFlags * flags) {
 			return this->to_uint64_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator char() const { 
-		return this->invoke<char>([&](_IDEC_flags * flags) {
+		return this->invoke<char>([&](ErrorFlags * flags) {
 			return this->to_int8_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator short() const { 
-		return this->invoke<short>([&](_IDEC_flags * flags) {
+		return this->invoke<short>([&](ErrorFlags * flags) {
 			return this->to_int16_xrnint (this->val, flags);
 		});
 	}
 
 	explicit operator int() const { 
-		return this->invoke<int>([&](_IDEC_flags * flags) {
+		return this->invoke<int>([&](ErrorFlags * flags) {
 			return this->to_int32_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator long() const { 
-		return this->invoke<long>([&](_IDEC_flags * flags) {
+		return this->invoke<long>([&](ErrorFlags * flags) {
 			return this->to_int64_xrnint (this->val, flags);
 		});
 	}
 	
 	explicit operator long long() const { 
-		return this->invoke<long long>([&](_IDEC_flags * flags) {
+		return this->invoke<long long>([&](ErrorFlags * flags) {
 			return this->to_int64_xrnint (this->val, flags);
 		});
 	}
 	
 	void inline operator+=(const Decimal<T> & other) {
-		this->val = this->invoke<D128>([&](_IDEC_flags * flags) {
-			return this->add(this->val, other.val, _IDEC_dflround, flags);
+		// TODO: WHAT IF ROUND MODES DON'T MATCH?
+		this->val = this->invoke<D128>([&](ErrorFlags * flags) {
+			return this->add(this->val, other.val, this->round_mode, flags);
 		});
 	}
 	
 	void inline operator-=(const Decimal<T> & other) {
-		this->val = this->invoke<D128>([&](_IDEC_flags * flags) {
-			return this->sub(this->val, other.val, _IDEC_dflround, flags);
+		this->val = this->invoke<D128>([&](ErrorFlags * flags) {
+			return this->sub(this->val, other.val, this->round_mode, flags);
 		});
 	}
 	
 	void inline operator*=(const Decimal<T> & other) {
-		this->val = this->invoke<D128>([&](_IDEC_flags * flags) {
-			return this->mul(this->val, other.val, _IDEC_dflround, flags);
+		this->val = this->invoke<D128>([&](ErrorFlags * flags) {
+			return this->mul(this->val, other.val, this->round_mode, flags);
 		});
 	}
 	
 	void inline operator/=(const Decimal<T> & other) {
-		this->val = this->invoke<D128>([&](_IDEC_flags * flags) {
-			return this->div(this->val, other.val, _IDEC_dflround, flags);
+		this->val = this->invoke<D128>([&](ErrorFlags * flags) {
+			return this->div(this->val, other.val, this->round_mode, flags);
 		});
 	}
 	
 	friend inline bool operator!=(const Decimal & l, const Decimal & r) { return !(l == r); }
 	friend inline bool operator==(const Decimal & l, const Decimal & r) { 
-		_IDEC_flags flags = _IDEC_allflagsclear;
+		ErrorFlags flags = Error::None;
 		auto result = l.quiet_equal (l.val, r.val, &flags);
 		return result > 0;
 	}
@@ -355,7 +352,7 @@ public:
 	friend inline bool operator<=(const Decimal & l, const Decimal & r) { return l < r || l == r; }
 	friend inline bool operator>=(const Decimal & l, const Decimal & r) { return l > r || l == r; }
 	friend inline bool operator<(const Decimal & l, const Decimal & r) {
-		_IDEC_flags flags = _IDEC_allflagsclear;
+		ErrorFlags flags = Error::None;
 		auto result = l.quiet_less (l.val, r.val, &flags);
 		return result > 0;
 	}
@@ -368,7 +365,46 @@ public:
 
 class Decimal128 : public Decimal<D128> {
 public:
-	Decimal128() {
+	Decimal128() : Decimal128(static_cast<int>(0)) { attach_bid_functions(); }
+	
+	Decimal128(const unsigned char value, const RoundMode round_mode = Round::NearestEven) 
+		: Decimal128((unsigned int) value, round_mode) {}
+	Decimal128(const unsigned short value, const RoundMode round_mode = Round::NearestEven) 
+		: Decimal128((unsigned int) value, round_mode) {}
+	Decimal128(const unsigned int value, const RoundMode round_mode = Round::NearestEven) 
+		: Decimal(round_mode) { 
+			this->attach_bid_functions();
+			this->val = this->from_uint32(value); 
+	}
+	Decimal128(const unsigned long value, const RoundMode round_mode = Round::NearestEven) 
+		: Decimal128((unsigned long long) value, round_mode) {}
+	Decimal128(const unsigned long long value, const RoundMode round_mode = Round::NearestEven) 
+		: Decimal(round_mode) { 
+			this->attach_bid_functions();
+			this->val = this->from_uint64(value); 
+	}
+	
+	Decimal128(const char value, const RoundMode round_mode = Round::NearestEven) : Decimal128((int) value, round_mode) {}
+	Decimal128(const short value, const RoundMode round_mode = Round::NearestEven) : Decimal128((int) value, round_mode) {}
+	Decimal128(const int value, const RoundMode round_mode = Round::NearestEven) : Decimal(round_mode) {
+		this->attach_bid_functions();
+	}
+	Decimal128(const long value, const RoundMode round_mode = Round::NearestEven) : Decimal128((long long) value, round_mode) {}
+	Decimal128(const long long value, const RoundMode round_mode = Round::NearestEven) : Decimal128(round_mode) { 
+		this->attach_bid_functions();
+		this->val = this->from_int64(value); }			
+	
+	Decimal128(const std::string & value, const RoundMode round_mode = Round::NearestEven) 
+		: Decimal(round_mode) {
+		this->attach_bid_functions();
+		auto val = value.empty()? "0" : value;
+		cstr c(val);
+		this->val = this->invoke<D128>([&](ErrorFlags * flags) {
+			return this->from_string(c.val, this->round_mode, flags);
+		});
+	}
+
+	void attach_bid_functions() {
 		this->from_string = bid128_from_string;
 		this->from_uint32 = bid128_from_uint32;
 		this->from_uint64 = bid128_from_uint64;
@@ -395,26 +431,6 @@ public:
 
 		this->val = this->from_uint64(0);
 	}
-	
-	Decimal128(const std::string & value) : Decimal128() {
-		auto val = value.empty()? "0" : value;
-		cstr c(val);
-		this->val = this->invoke<D128>([&](_IDEC_flags * flags) {
-			return this->from_string(c.val, _IDEC_dflround, flags);
-		});
-	}
-	
-	Decimal128(const unsigned char value) : Decimal128((unsigned int) value) {}
-	Decimal128(const unsigned short value) : Decimal128((unsigned int) value) {}
-	Decimal128(const unsigned int value) : Decimal128() { this->val = this->from_uint32(value); }
-	Decimal128(const unsigned long value) : Decimal128((unsigned long long) value) {}
-	Decimal128(const unsigned long long value) : Decimal128() { this->val = this->from_uint64(value); }
-	
-	Decimal128(const char value) : Decimal128((int) value) {}
-	Decimal128(const short value) : Decimal128((int) value) {}
-	Decimal128(const int value) : Decimal128() { this->val = this->from_int32(value); }
-	Decimal128(const long value) : Decimal128((long long) value) {}
-	Decimal128(const long long value) : Decimal128() { this->val = this->from_int64(value); }			
 	
 	friend inline Decimal128 abs(const Decimal128 & v) {
 		Decimal128 v2 = v;
